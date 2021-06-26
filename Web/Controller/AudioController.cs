58e18d1b7b42bc5af55c8d45d2bd6a09632bba44
@Kinda_Flihan <kinda.flaihan.1998@gmail.com>
2021-06-26 12:40:28 +0300

    IR-119 Add controller and services file for audio.

---
+++ Web/Controller/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using iread_interaction_ms.Web.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iread_interaction_ms.Web.Controller
+{
+    public class AudioController:ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly AudioServices _audioServices;
+
+        public AudioController(AudioServices audioServices, IMapper mapper)
+        {
+            _audioServices = audioServices;
+            _mapper = mapper;
+        }
+        
+        
+    }
+}
---
+++ Web/Service/AudioServices.cs
@@ -0,0 +1,14 @@
+﻿using iread_interaction_ms.DataAccess.Interface;
+
+namespace iread_interaction_ms.Web.Service
+{
+    public class AudioServices
+    {
+        private readonly IAudioRepository _audioRepository;
+
+        public AudioServices(IAudioRepository audioRepository)
+        {
+            _audioRepository = audioRepository;
+        }
+    }
+}
