9952b7679abba7842c6c519780524995f9e2dff3
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-09 12:00:19 +0300

    get by page drawings

--- DataAccess/Interface/IDrawingRepository.cs
+++ DataAccess/Interface/IDrawingRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using iread_interaction_ms.DataAccess.Data.Entity;
 
 namespace iread_interaction_ms.DataAccess.Interface
@@ -6,14 +7,11 @@ namespace iread_interaction_ms.DataAccess.Interface
     public interface IDrawingRepository
     {
         public Task<Drawing> GetById(int id);
-        
         public void Insert(Drawing drawing);
-        
         public void Delete(Drawing drawing);
-
         public bool Exists(int id);
-
         public void Update(Drawing drawing, Drawing oldDrawing);
         public Task<Drawing> GetByInteractionId(int id);
+        Task<List<Drawing>> GetByPageId(int pageId);
     }
 }
