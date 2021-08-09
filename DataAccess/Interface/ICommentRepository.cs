5c880d8bc423bb737ed53587c734146019fa422b
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-09 11:55:11 +0300

    get by page comment

--- DataAccess/Interface/ICommentRepository.cs
+++ DataAccess/Interface/ICommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using iread_interaction_ms.DataAccess.Data.Entity;
 
 namespace iread_interaction_ms.DataAccess.Interface
@@ -6,14 +7,17 @@ namespace iread_interaction_ms.DataAccess.Interface
     public interface ICommentRepository
     {
         public Task<Comment> GetById(int id);
-        
+
         public void Insert(Comment comment);
-        
+
         public void Delete(Comment comment);
 
         public bool Exists(int id);
 
         public void Update(Comment comment, Comment oldComment);
+
         public Task<Comment> GetByInteractionId(int id);
+
+        public Task<List<Comment>> GetByPageId(int pageId);
     }
 }
--- Web/Controller/CommentController.cs
+++ Web/Controller/CommentController.cs
@@ -17,7 +17,7 @@ namespace iread_interaction_ms.Web.Controller
 {
     [ApiController]
     [Route("api/Interaction/[controller]/")]
-    public class CommentController:ControllerBase
+    public class CommentController : ControllerBase
     {
         private readonly IMapper _mapper;
         private readonly CommentsService _commentsService;
@@ -31,12 +31,12 @@ namespace iread_interaction_ms.Web.Controller
             _interactionServices = interactionServices;
             _consulHttpClient = consulHttpClient;
         }
-        
+
         // GET: api/interaction/comment/1/
         [HttpGet("{id}/get")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetById([FromRoute]int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
             Comment comment = await _commentsService.GetById(id);
 
@@ -52,7 +52,7 @@ namespace iread_interaction_ms.Web.Controller
         [HttpGet("get-by-interaction/{id}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetByInteractionId([FromRoute]int id)
+        public async Task<IActionResult> GetByInteractionId([FromRoute] int id)
         {
             Comment comment = await _commentsService.GetByInteractionId(id);
 
@@ -62,6 +62,22 @@ namespace iread_interaction_ms.Web.Controller
             }
 
             return Ok(_mapper.Map<CommentDto>(comment));
+        }
+
+        // GET: api/interaction/comment/get-by-page/1
+        [HttpGet("get-by-page/{pageId}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetByPageId([FromRoute] int pageId)
+        {
+            List<Comment> comments = await _commentsService.GetByPageId(pageId);
+
+            if (comments == null || !comments.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<List<CommentDto>>(comments));
         }
 
 
@@ -84,7 +100,7 @@ namespace iread_interaction_ms.Web.Controller
                 return BadRequest(ErrorMessage.ModelStateParser(ModelState));
             }
 
-           
+
             if (!_commentsService.Insert(comment))
             {
                 return BadRequest();
@@ -97,26 +113,26 @@ namespace iread_interaction_ms.Web.Controller
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Update([FromBody] CommentUpdateDto comment, [FromRoute] int id)
         {
-             if (comment == null)
+            if (comment == null)
             {
                 return BadRequest();
-            }            
-            
+            }
+
             Comment oldComment = _commentsService.GetById(id).Result;
-             if (oldComment == null)
+            if (oldComment == null)
             {
                 return NotFound();
             }
 
             // get word from database before checking
             Comment commentEntity = _mapper.Map<Comment>(comment);
-            commentEntity.Word = oldComment.Word;            
+            commentEntity.Word = oldComment.Word;
             ValidationLogic(commentEntity);
             if (!ModelState.IsValid)
             {
                 return BadRequest(ErrorMessage.ModelStateParser(ModelState));
             }
-            
+
             commentEntity.CommentId = id;
             _commentsService.Update(commentEntity, oldComment);
             return NoContent();
@@ -138,56 +154,63 @@ namespace iread_interaction_ms.Web.Controller
                 return NotFound();
             }
 
-           _commentsService.Delete(comment);
+            _commentsService.Delete(comment);
             return NoContent();
         }
 
 
-    private void ValidationLogicForAdding(Comment comment)
-    {
-
-        ViewStoryDto storyDto = _consulHttpClient.GetAsync<ViewStoryDto>("story_ms", $"/api/story/get/{comment.Interaction.StoryId}").Result;
-
-        if(storyDto == null || storyDto.StoryId < 1){
-             ModelState.AddModelError("StoryId", "Story not found");    
-        }
-
-        PageDto pageDto = _consulHttpClient.GetAsync<PageDto>("story_ms", $"/api/story/Page/get/{comment.Interaction.PageId}").Result;
-
-        if(pageDto == null || pageDto.PageId < 1){
-             ModelState.AddModelError("PageId", "Page not found");    
-        }
-
-        UserDto userDto = _consulHttpClient.GetAsync<UserDto>("identity_ms", $"/api/identity_ms/SysUsers/{comment.Interaction.StudentId}/get").Result;
-
-        if(userDto == null || string.IsNullOrEmpty(userDto.Id)){
-             ModelState.AddModelError("StudentId", "Student not found");    
-        }
-        else
+        private void ValidationLogicForAdding(Comment comment)
         {
-            if(!userDto.Role.Equals(RoleTypes.Student.ToString()))
+
+            ViewStoryDto storyDto = _consulHttpClient.GetAsync<ViewStoryDto>("story_ms", $"/api/story/get/{comment.Interaction.StoryId}").Result;
+
+            if (storyDto == null || storyDto.StoryId < 1)
             {
-             ModelState.AddModelError("StudentId", "User not a student");    
+                ModelState.AddModelError("StoryId", "Story not found");
+            }
+
+            PageDto pageDto = _consulHttpClient.GetAsync<PageDto>("story_ms", $"/api/story/Page/get/{comment.Interaction.PageId}").Result;
+
+            if (pageDto == null || pageDto.PageId < 1)
+            {
+                ModelState.AddModelError("PageId", "Page not found");
+            }
+
+            UserDto userDto = _consulHttpClient.GetAsync<UserDto>("identity_ms", $"/api/identity_ms/SysUsers/{comment.Interaction.StudentId}/get").Result;
+
+            if (userDto == null || string.IsNullOrEmpty(userDto.Id))
+            {
+                ModelState.AddModelError("StudentId", "Student not found");
+            }
+            else
+            {
+                if (!userDto.Role.Equals(RoleTypes.Student.ToString()))
+                {
+                    ModelState.AddModelError("StudentId", "User not a student");
+                }
             }
         }
-    }
 
-    private void ValidationLogic(Comment comment)
+        private void ValidationLogic(Comment comment)
         {
-            if(comment.CommentType.Equals(CommentType.WORD_CLASS.ToString())){
-                
-                if(!WordClasses.elementsAsStr.Contains(comment.Value)){
-                    ModelState.AddModelError("Value", "Value should be one of [" + string.Join(",", WordClasses.elementsAsStr) +"]");    
+            if (comment.CommentType.Equals(CommentType.WORD_CLASS.ToString()))
+            {
+
+                if (!WordClasses.elementsAsStr.Contains(comment.Value))
+                {
+                    ModelState.AddModelError("Value", "Value should be one of [" + string.Join(",", WordClasses.elementsAsStr) + "]");
                 }
             }
 
-            if(comment.CommentType.Equals(CommentType.EXAMPLE.ToString())){
-                List<string> wordsOfExample = comment.Value.Split(' ',',', '.', ':', '\t').ToList();
-                List<string> delimiter = new List<string>(){"",",", ".", ":", "\t"};
+            if (comment.CommentType.Equals(CommentType.EXAMPLE.ToString()))
+            {
+                List<string> wordsOfExample = comment.Value.Split(' ', ',', '.', ':', '\t').ToList();
+                List<string> delimiter = new List<string>() { "", ",", ".", ":", "\t" };
                 wordsOfExample.RemoveAll(word => delimiter.Contains(word));
-                
-                if(wordsOfExample.Count() < 2 || !wordsOfExample.Contains(comment.Word)){
-                    ModelState.AddModelError("Value", $"Value should be a sentence and contains the word \'{comment.Word}\'");    
+
+                if (wordsOfExample.Count() < 2 || !wordsOfExample.Contains(comment.Word))
+                {
+                    ModelState.AddModelError("Value", $"Value should be a sentence and contains the word \'{comment.Word}\'");
                 }
             }
 
