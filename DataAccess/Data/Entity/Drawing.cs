5ea05c1fed0c22aea7043dcc875668662c6d8bae
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-22 14:27:33 +0300

    add color to drawing

--- DataAccess/Data/Entity/Drawing.cs
+++ DataAccess/Data/Entity/Drawing.cs
@@ -12,14 +12,14 @@ namespace iread_interaction_ms.DataAccess.Data.Entity
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
         public int DrawingId { get; set; }
-        
+
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "The InteractionId field is required.")]
         public int InteractionId { get; set; }
         public Interaction Interaction { get; set; }
-        
+
         public int AudioId { get; set; }
-        
+
         public string Comment { get; set; }
 
         [Required(AllowEmptyStrings = false)]
@@ -28,6 +28,9 @@ namespace iread_interaction_ms.DataAccess.Data.Entity
         public int MaxY { get; set; }
         public int MinX { get; set; }
         public int MinY { get; set; }
-        
+        [Required(AllowEmptyStrings = false)]
+        public string Color { get; set; }
+
+
     }
 }
