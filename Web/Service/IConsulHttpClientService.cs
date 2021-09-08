a3b5a5cc4909212f807d46e9383457e23635c32a
yazan.codavia <yazankassam.codavia@gmail.com>
2021-09-08 14:22:09 +0300

    delete related attachments

--- Web/Service/IConsulHttpClientService.cs
+++ Web/Service/IConsulHttpClientService.cs
@@ -9,6 +9,7 @@ namespace iread_interaction_ms.Web.Service
     public interface IConsulHttpClientService
     {
         Task<T> GetAsync<T>(string serviceName, string requestUri);
+        Task<bool> Delete(string serviceName, string requestUri);
         AgentService GetAgentService(string serviceName);
         Task<T> PostBodyAsync<T>(string serviceName, string requestUri, Object obj);
         Task<T> PostFormAsync<T>(string serviceName, string requestUri, Dictionary<string, string> parameters,
