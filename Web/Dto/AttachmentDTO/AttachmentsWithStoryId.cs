0b57fe6c44632b3c5c1c3aa9565f5561a412a7f0
Samir Ahmad <samir.ahmad@medicus.ai>
2023-11-18 15:04:28 +0300

    Change remote

---
+++ Web/Dto/AttachmentDTO/AttachmentsWithStoryId.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace iread_interaction_ms.Web.Dto.AttachmentDto
+{
+    public class AttachmentsWithStoryId
+    {
+        public AttachmentsWithStoryId(int storyId, List<IFormFile> attachment)
+        {
+            this.Attachment = attachment;
+            this.StoryId = storyId;
+        }
+
+        public int StoryId;
+        public List<IFormFile> Attachment;
+    }
+}
